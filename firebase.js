283f1691bbd44ce70ef392d4eb2acb480af83ab4
kdkfkfkfkfkxk <aymenhu186@gmail.com>
2025-04-18 05:49:46 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKiHADVuRqq1Mse2h2ohDeKLM9_rvmrhY",
+  authDomain: "kr0wl-chat.firebaseapp.com",
+  databaseURL: "https://kr0wl-chat-default-rtdb.firebaseio.com",
+  projectId: "kr0wl-chat",
+  storageBucket: "kr0wl-chat.appspot.com",
+  messagingSenderId: "941342507383",
+  appId: "1:941342507383:web:50d6c755abe6aef1066172",
+  measurementId: "G-WZ8NME39V7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
